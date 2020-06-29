f04e18c08f99c26bad3e14edfc6b7dfa50e070ea
Maks <66674521+jjestertt@users.noreply.github.com>
2020-06-30 00:26:06 +0200

    Begin

---
+++ justStudy/app.js
@@ -0,0 +1,44 @@
+// //Возведение массива в степень
+// const arr = [1, 2, 3, 4, 5];
+// let arrToPow = (parametr = prompt('Введите степень')) => {
+//     for (let i = 0; i < arr.length; i++) {
+//         arr[i] = arr[i] ** parametr;
+//     }
+//     return arr;
+// }
+// console.log(arrToPow());
+
+
+
+// // Вывод четных чиел из массива
+// let numbes = (arr) => {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] % 2 == 0) { //Остаток деления на 2 = 0 
+//             console.log(arr[i]);
+//         }
+//     }
+// }
+// const y = [];
+// // ты сам задашь массив
+// for (let i = 0; i < 10; i++) y.push(prompt(`Введи значени массива ${i}`));
+// numbes(y);
+
+
+// Счетчик For и While
+// // for (let i = 0; i < 3; i++) {
+// //     console.log(`number ${i}!`);
+// // }
+// let i = 0;
+// while (i < 3) {
+//     console.log(`number ${i}!`);
+//     i++;
+// }
+
+
+
+//простое если при делении на любое число кроме 1 и n есть остаток.
+
+let n = 10;
+
+
+
