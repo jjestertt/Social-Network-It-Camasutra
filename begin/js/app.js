19c2daafdea2e5e14b704f3cafc21651a33217ff
Maks <66674521+jjestertt@users.noreply.github.com>
2020-07-02 17:42:47 +0200

    add title

--- begin/js/app.js
+++ begin/js/app.js
@@ -1,7 +1,18 @@
-const titleBtn = document.querySelector('#title-btn');
-
-let anyFunction = () => {
-
+const headerBtn = document.querySelector('#header-btn');
+const titleContent = document.querySelector('#title-content');
+//Функция быстрого вывода 
+let addToDocument = (parametr) => {
+    let textNode = document.createTextNode(parametr);
+    titleContent.appendChild(textNode);
 }
 
-titleBtn.addEventListener('click', anyFunction);
+let clickFunction = () => {
+    let temp = '';
+    for (let i = 1; i < 9; i++) {
+        temp = temp + `7 * ${i} = ${7 * i}`;
+    }
+
+    addToDocument(temp);
+}
+
+headerBtn.addEventListener('click', clickFunction);
