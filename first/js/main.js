c504c6d9e8b1a5b3c6b07b146329fb1039871313
Maks <66674521+jjestertt@users.noreply.github.com>
2020-08-02 01:20:53 +0200

    agan

--- first/js/main.js
+++ first/js/main.js
@@ -1,7 +1,31 @@
-let btn = document.querySelector('.btn__wrap');
-let btnIn = document.querySelector('.btn__in')
+let menuBtn = document.querySelector('.header__menu-btn-wrapper');
+let menuBtnIn = document.querySelector('.header__menu-btn-inner')
+let menuItem = document.querySelector('.header__menu-list');
+let body = document.querySelector('body');
 let toggleClass = () => {
-    btn.classList.toggle('btn__wrap_active');
-    btnIn.classList.toggle('btn__in_active')
+    menuBtn.classList.toggle('header__menu-btn-wrapper_active');
+    menuBtnIn.classList.toggle('header__menu-btn-inner_active');
+    menuItem.classList.toggle('header__menu-list_active');
+    body.classList.toggle('body__lock-scroll');
 }
-btn.addEventListener('click', toggleClass);
+menuBtn.addEventListener('click', toggleClass);
+
+//tabs 
+let tabBtn = document.querySelectorAll('.tabs__change');
+let tabText = document.querySelectorAll('.tabs__center');
+
+for (let i = 0; i < tabBtn.length; i++) {
+    if (i != 0) {
+        tabText[i].style.display = 'none';
+    }
+    tabBtn[i].addEventListener('click', () => {
+        for (let j = 0; j < tabBtn.length; j++) {
+            if (j != i) {
+                tabText[j].style.display = 'none';
+                tabBtn[j].classList.remove('tabs__active');
+            }
+        }
+        tabText[i].style.display = 'block'
+        tabBtn[i].classList.add('tabs__active');
+    })
+}
