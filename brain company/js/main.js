97cc93e80f48c5360e3ad9a99e273838dee2b4a1
Maks <66674521+jjestertt@users.noreply.github.com>
2020-08-07 00:50:36 +0200

    123

---
+++ brain company/js/main.js
@@ -0,0 +1,18 @@
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
+$(document).ready(function () {
+    $('.feedback__slider').slick({
+        infinite: false,
+        arrows: false
+    });
+});
