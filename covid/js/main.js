e535c53f6fe0f0fb2f12c6cc8fb720acccbebbe8
Maks <66674521+jjestertt@users.noreply.github.com>
2020-08-25 16:03:24 +0200

    new section trust

--- covid/js/main.js
+++ covid/js/main.js
@@ -1,9 +1,11 @@
-// menu
+// show mobile menu
 let menu = document.querySelector('.header__menu');
 let menuBtn = document.querySelector('.header__menu-icon');
+let body = document.querySelector('body');
 
 let showMenu = () => {
     menu.classList.toggle('header__menu-active');
+    body.classList.toggle('overflow');
 }
 
 menuBtn.addEventListener('click', showMenu);
