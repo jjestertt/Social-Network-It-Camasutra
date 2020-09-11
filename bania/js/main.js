48114faee7209ae7c50538bb3e29e10aaa72dffd
Maks <66674521+jjestertt@users.noreply.github.com>
2020-09-11 21:07:23 +0200

    123

--- bania/js/main.js
+++ bania/js/main.js
@@ -1,5 +1,5 @@
 // scroll
-const smoothLinks = document.querySelectorAll('a[href^="#"]');
+const smoothLinks = document.querySelectorAll('.scroll-to');
 for (let smoothLink of smoothLinks) {
     smoothLink.addEventListener('click', function (e) {
         e.preventDefault();
@@ -12,3 +12,25 @@ for (let smoothLink of smoothLinks) {
     });
 };
 //scroll
+
+//popap
+let getPopapButton = document.querySelectorAll('.popap-btn');
+let closePopapButton = document.querySelector('.popap__button-close');
+let popapWindow = document.querySelector('.popap');
+
+//show popap
+let showPopap = (e) => {
+    e.preventDefault();
+    popapWindow.classList.remove('popap__hide');
+}
+
+//hide popap
+let hidePopap = (e) => {
+    e.preventDefault();
+    popapWindow.classList.add('popap__hide');
+}
+
+for (i = 0; i < getPopapButton.length; i++) {
+    getPopapButton[i].addEventListener('click', showPopap);
+}
+closePopapButton.addEventListener('click', hidePopap);
