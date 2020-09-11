eaef4fe52cecbd67ae193dde261be971d3e3654f
Maks <66674521+jjestertt@users.noreply.github.com>
2020-09-12 00:34:15 +0200

    optimization all

--- bania/js/main.js
+++ bania/js/main.js
@@ -35,7 +35,7 @@ for (i = 0; i < getPopapButton.length; i++) {
 }
 closePopapButton.addEventListener('click', hidePopap);
 
-
+//animation
 AOS.init({
     // Global settings:
     disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
@@ -49,7 +49,7 @@ AOS.init({
     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
     offset: 100, // offset (in px) from the original trigger point
     delay: 0, // values from 0 to 3000, with step 50ms
-    duration: 500, // values from 0 to 3000, with step 50ms
+    duration: 450, // values from 0 to 3000, with step 50ms
     easing: 'ease', // default easing for AOS animations
     once: true, // whether animation should happen only once - while scrolling down
     mirror: false, // whether elements should animate out while scrolling past them
