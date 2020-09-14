0af2d4099ae601f7d4f2d31352324744d5a8668e
Maks <66674521+jjestertt@users.noreply.github.com>
2020-09-15 00:14:29 +0200

    clean

--- WP Наработки/Повторитель.php
+++
@@ -1,3 +0,0 @@
-<?php if( have_rows('header__menu') ): while ( have_rows('header__menu') ) : the_row(); ?>
-    <li class="header__menu-item"><a href="<?php the_sub_field('header__menu-item-link'); ?>"><?php the_sub_field('header__menu-item-name'); ?></a></li>
-<?php endwhile; endif; ?>
