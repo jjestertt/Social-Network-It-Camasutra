1b9a5e3c26e6accf79fbf9bf82a706b4ed51a434
Maks <jjestertt@gmail.com>
2020-09-27 21:00:42 +0200

    new commit

---
+++ To do/js/main.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('.add-task__form');
+const input = document.querySelector('.add-task__form-text');
+
+
+form.addEventListener('submit', (evt) => {
+   evt.preventDefault();
+
+   const dataTask = input.value;
+   const htmlTask = 
+
+});
